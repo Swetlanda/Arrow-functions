3fc0aac6652a00c7418d5ddcaf060daf0b25faa8
Lola Crispin <swetlanda@gmail.com>
2024-06-25 12:04:41 +0000

    Arrow function

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,37 @@
  */
 
 // Vanilla JavaScript Function
+//function addTwoNumbers (a,b) {
+//    return a+b;
+//}
+
+//let sum =addTwoNumbers(3,5);
+//console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a,b) => {
+    return a+b;
+}
+
+let sum =addTwoNumbers(3,5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2=(a,b) => a+b;
+let sum2 =addTwoNumbers(3,5);
+console.log(sum2);
 
 // Implicit Returns
+const saySmth = message=>console.log(message);
+saySmth("Hello there!")
+
+const sayHello = () => console.log ('Hello');
+ sayHello()
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiple string!
+    </p>`
+)
+console.log(returnMultipleLines);
